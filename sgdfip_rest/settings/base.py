4fb6578ded68175e3b46a9d4eda5e2a5b2160a87
cucarach0n <luis_ls11@hotmail.com>
2022-02-05 16:29:34 -0500

    fix problemas de seguridad

--- sgdfip_rest/settings/base.py
+++ sgdfip_rest/settings/base.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8r*yvt7^=0^47*y)555aeu)6tv^!x-s_lna)$gg2!ioznvi%q('
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
